ef0c51d1ed0fbf5cfe60839d5dfa3743752b5425
Linas819 <63401219+Linas819@users.noreply.github.com>
2023-06-30 10:49:49 +0300

    Creadet game logic to read JSON data an add points in game, based on coordinates

---
+++ Game Task/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+    Levels gameLevels;
+    public string levelProgression;
+    public GameObject pointObject, canvas;
+    public List<GameObject> points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameLevels = new Levels();
+        ReadData(); //reads data from Json file
+    }
+
+    void ReadData()
+    {
+        string DataLocation = "C:/Users/Linas/GameTaskRepo/GameTask/Game Task/Assets/GameAssets/level_data.json"; // the location of the json file in build
+        string jsonString = File.ReadAllText(DataLocation); // reading the json file
+        gameLevels = JsonUtility.FromJson<Levels>(jsonString); //attaching the Json file's data to a Levels object vriable
+    }
+
+    public void CreateLevel(int currentLevel)
+    {
+        int currentPoint = 0; //variable used to track the current point ebing added in a level
+        for (int i = 0; i < gameLevels.levels[currentLevel].levelData.Length; i = i + 2)
+        {
+            points.Add(Instantiate(pointObject, new Vector2(float.Parse(gameLevels.levels[currentLevel].levelData[i]), float.Parse(gameLevels.levels[currentLevel].levelData[i + 1]) * -1), Quaternion.identity));
+            points[currentPoint].GetComponentInChildren<TextMeshProUGUI>().text = (currentPoint + 1).ToString(); //seting the numeric value of the boind to a text gameobject
+            points[currentPoint].transform.SetParent(canvas.transform, false); //transforming the current point game object, to be the child of the canvas game object.
+            currentPoint++;//setting up the new point's index 
+        }
+    }
+    [System.Serializable]
+    public class LevelData
+    {
+        public string[] levelData;
+    }
+    [System.Serializable]
+    public class Levels
+    {
+        public LevelData[] levels;
+    }
+}
